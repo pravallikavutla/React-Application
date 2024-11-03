b5757c03c0bee21bd49307c54fd37e27dbcb236b
pravallikavutla <113453485+pravallikavutla@users.noreply.github.com>
2024-11-03 15:35:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,6 @@ import React from 'react'
 
 
 
-import Home from './Navbar/Home';
-import Navv from './Navbar/Navv';
-import {Routes, Route } from "react-router-dom"
-import Login from './Navbar/Login';
-import About from './Navbar/About';
-import Profile from './Navbar/Profile';
-import Signup from './Navbar/Signup';
-import FetchDaata from './Axios/FetchDaata';
 import Heading from './FinalProject/Heading';
 import Weather from './FinalProject/Weather';
 
@@ -22,53 +14,7 @@ export default function App() {
 
       <Heading/>
       <Weather/>
-{/* <FetchDaata/> */}
-{/* <Navv/> */}
-  {/* <Home/>
-  <Login/>
-  <About/>
-  <Profile/>
-  <Signup/>                                                                                    */}
-  {/* <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/login" element={<Login/>}></Route>
-    <Route path="/profile" element={<Profile/>}></Route>
-    <Route path="/signup" element={<Signup/>}></Route>
-    
-  </Routes> */}
-   {/* <Component1/>    */}
 
-{/* <Component1/> */}
-{/* <Component2/> */}
-
-     
-     
-  
-      {/* <Stateexample8/> */}
-  {/* <StateExample7/>  */}
-{/* <Form/> */}
-{/* <StateExample6/> */}
-
-
-{/* <State1/> */}
-
-
-
-      
-     {/* <StateExample4/>
-      <StateExample5/>  */}
-
-       {/* <Example/> */}
-      {/* <ParentComponent/> */}
-
-
-
-
-
-
-      {/* <Component1/> */}
-     {/* <Component1/> */}
 
    </div>
    
