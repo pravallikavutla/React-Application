cd534e80b5506dabd7edd18caadf48b68469447a
pravallikavutla <113453485+pravallikavutla@users.noreply.github.com>
2024-11-03 15:27:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Heading from './FinalProject/Heading';
+import Weather from './FinalProject/Weather';
+
+
+export default function App() {
+  
+  return (
+    <div className='app'>
+
+      <Heading/>
+      <Weather/>
+     </div>
+   
+  )
+
+}
