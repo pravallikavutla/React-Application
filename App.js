9395303b62e52229df641f51263a00226f57f481
pravallikavutla <113453485+pravallikavutla@users.noreply.github.com>
2024-11-03 15:29:28 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,16 @@
 import React from 'react'
+
+
+
+
+import Home from './Navbar/Home';
+import Navv from './Navbar/Navv';
+import {Routes, Route } from "react-router-dom"
+import Login from './Navbar/Login';
+import About from './Navbar/About';
+import Profile from './Navbar/Profile';
+import Signup from './Navbar/Signup';
+import FetchDaata from './Axios/FetchDaata';
 import Heading from './FinalProject/Heading';
 import Weather from './FinalProject/Weather';
 
@@ -10,7 +22,55 @@ export default function App() {
 
       <Heading/>
       <Weather/>
-     </div>
+{/* <FetchDaata/> */}
+{/* <Navv/> */}
+  {/* <Home/>
+  <Login/>
+  <About/>
+  <Profile/>
+  <Signup/>                                                                                    */}
+  {/* <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/profile" element={<Profile/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+    
+  </Routes> */}
+   {/* <Component1/>    */}
+
+{/* <Component1/> */}
+{/* <Component2/> */}
+
+     
+     
+  
+      {/* <Stateexample8/> */}
+  {/* <StateExample7/>  */}
+{/* <Form/> */}
+{/* <StateExample6/> */}
+
+
+{/* <State1/> */}
+
+
+
+      
+     {/* <StateExample4/>
+      <StateExample5/>  */}
+
+       {/* <Example/> */}
+      {/* <ParentComponent/> */}
+
+
+
+
+
+
+      {/* <Component1/> */}
+     {/* <Component1/> */}
+
+   </div>
    
   )
 
