bb7a335ff7dcafd646eaa0e1e82704cc7b39dd63
pravallikavutla <113453485+pravallikavutla@users.noreply.github.com>
2024-11-03 15:25:54 +0530

    Create Heading.js

---
+++ Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Heading.css'
+function Heading() {
+  return (
+    <div>
+      <h1 className="heading">WEATHER REPORT APPLICATION</h1>
+    </div>
+  )
+}
+
+export default Heading
